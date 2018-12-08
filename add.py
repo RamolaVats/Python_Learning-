8df545896da8db6cb4770bc9281926602a7347ad
RamolaVats <vatsramola26@gmail.com>
2018-12-08 12:04:55 +0530

    adding two numbers

---
+++ add.py
@@ -0,0 +1,11 @@
+def sum(x,y):
+    return x+y
+
+#def avg(x,y):
+ #   return sum(x,y)//2
+
+a= int(input("Enter first number:"))
+b= int(input("Enter second number:"))
+
+print("Sum of the given two numbers is: ", sum(a,b))
+#print("Average of the given numbers is: ", avg(a,b))
